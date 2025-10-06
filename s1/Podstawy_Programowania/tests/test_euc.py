58bce2f4a8b55e978272c523ad9b7c4e6dd20507
wiciu <w.szylinski@op.pl>
2025-10-06 15:55:44 +0200

    feat: passed test module

---
+++ s1/Podstawy_Programowania/tests/test_euc.py
@@ -0,0 +1,20 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+from s1.Podstawy_Programowania.euclidean_algorithm import EUC, EUC_MOD
+
+def test_euc():
+    assert EUC(42,18) == 6
+    assert EUC_MOD(18, 42) == 6
+    assert EUC_MOD(10, 5) == 5
+    assert EUC_MOD(17, 13) == 1
+    assert EUC_MOD(0, 9) == 9
+    assert EUC_MOD(9, 0) == 9
+
+def test_euc_mod():
+    assert EUC_MOD(42, 18) == 6
+    assert EUC_MOD(18, 42) == 6
+    assert EUC_MOD(10, 5) == 5
+    assert EUC_MOD(17, 13) == 1
+    assert EUC_MOD(0, 9) == 9
+    assert EUC_MOD(9, 0) == 9
