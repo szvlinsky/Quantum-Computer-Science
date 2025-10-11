303b429178659e20a9dee66a3dd7160cfd2ea223
wiciu <w.szylinski@op.pl>
2025-10-11 21:21:41 +0200

    fix: delete prints

--- semester_01/algorithms_and_data_structures/scripts/swap_algorithm/swap_algorithm.c
+++ semester_01/algorithms_and_data_structures/scripts/swap_algorithm/swap_algorithm.c
@@ -3,14 +3,10 @@
 int main() {
     int a = 2;
     int b = 3; //pomijamy operacje początkowe
-
-    printf("Poczatkowo: a = %d, b = %d\n", a, b);
-
+    
     a = a + b; //2 operacje
     b = a - b; //2 operacje
     a = a - b; //2 operacje
-
-    printf("Po zamianie: a = %d, b = %d\n", a, b);
 
     return 0; //łącznie 6 operacji
 }
