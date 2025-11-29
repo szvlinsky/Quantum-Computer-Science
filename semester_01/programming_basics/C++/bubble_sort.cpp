8632639d5d8ee43b63e7a606aac745df366e448e
wiciu <w.szylinski@op.pl>
2025-11-29 21:30:58 +0100

    feat: add bubble sort algorithm

---
+++ semester_01/programming_basics/C++/bubble_sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[1000];
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - 1 - i; ++j) {
+            if (a[j] > a[j + 1]) {
+                int tmp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
