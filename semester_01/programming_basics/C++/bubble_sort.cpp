a259ccc71e427f79afa4fecaf6bd3959b918305b
wiciu <w.szylinski@op.pl>
2025-11-29 22:25:38 +0100

    fix: incrementation update

--- semester_01/programming_basics/C++/bubble_sort.cpp
+++ semester_01/programming_basics/C++/bubble_sort.cpp
@@ -6,12 +6,12 @@ int main() {
     cin >> n;
     int a[1000];
 
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
 
-    for (int i = 0; i < n - 1; ++i) {
-        for (int j = 0; j < n - 1 - i; ++j) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
             if (a[j] > a[j + 1]) {
                 tmp = a[j];
                 a[j] = a[j + 1];
@@ -20,7 +20,7 @@ int main() {
         }
     }
 
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         cout << a[i] << " ";
     }
     return 0;
