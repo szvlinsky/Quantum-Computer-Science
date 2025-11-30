a2141d7b09b15ee599ee1f9bbb4ae7c368540d19
wiciu <w.szylinski@op.pl>
2025-11-30 19:51:40 +0100

    feat: add sort algorithm

--- semester_01/programming_basics/C++/select_sort.cpp
+++ semester_01/programming_basics/C++/select_sort.cpp
@@ -9,5 +9,17 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
+
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (a[j] < a[min_idx]) {
+                min_idx = j;
+            }
+        }
+        int tmp = a[i];
+        a[i] = a[min_idx];
+        a[min_idx] = tmp;
+    }
     return 0;
 }
